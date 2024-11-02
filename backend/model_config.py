cf6bab9a5ba2968125201d7f3aa1dfb890453103
dharllc <151599752+dharllc@users.noreply.github.com>
2024-11-02 16:24:42 -0700

    hide unused models

--- backend/model_config.py
+++ backend/model_config.py
@@ -1,11 +1,11 @@
 MODELS = {
     "OpenAI": {
-        "o1-preview-2024-09-12": {"input": 15, "output": 60, "input_tokens": 128000, "output_tokens": 32768},
+        # "o1-preview-2024-09-12": {"input": 15, "output": 60, "input_tokens": 128000, "output_tokens": 32768},
         "o1-mini-2024-09-12": {"input": 3, "output": 12, "input_tokens": 128000, "output_tokens": 65536},
-        "gpt-4o-2024-05-13": {"input": 5, "output": 15, "input_tokens": 128000, "output_tokens": 4096},
+        # "gpt-4o-2024-05-13": {"input": 5, "output": 15, "input_tokens": 128000, "output_tokens": 4096},
         "gpt-4o-2024-08-06": {"input": 5, "output": 15, "input_tokens": 128000, "output_tokens": 16384},
-        "chatgpt-4o-latest": {"input": 5, "output": 15, "input_tokens": 128000, "output_tokens": 16384},
-        "gpt-4o-mini-2024-07-18": {"input": 0.15, "output": 0.60, "input_tokens": 128000, "output_tokens": 16384}
+        # "chatgpt-4o-latest": {"input": 5, "output": 15, "input_tokens": 128000, "output_tokens": 16384},
+        # "gpt-4o-mini-2024-07-18": {"input": 0.15, "output": 0.60, "input_tokens": 128000, "output_tokens": 16384}
     },
     "Anthropic": {
         "claude-3-5-sonnet-20241022": {"input": 3, "output": 15, "input_tokens": 200000, "output_tokens": 8192},
@@ -15,10 +15,10 @@ MODELS = {
     },
     "Google": {
         "gemini-1.5-pro": {"input": 3.50, "output": 10.50, "input_tokens": 2097152, "output_tokens": 8192},
-        "gemini-1.5-pro-exp-0801": {"input": 3.50, "output": 10.50, "input_tokens": 2097152, "output_tokens": 8192},
-        "gemini-1.5-pro-exp-0827": {"input": 3.50, "output": 10.50, "input_tokens": 2097152, "output_tokens": 8192},
-        "gemini-1.5-flash": {"input": 0.075, "output": 0.30, "input_tokens": 1048576, "output_tokens": 8192},
-        "gemini-1.5-flash-exp-0827": {"input": 0.075, "output": 0.30, "input_tokens": 1048576, "output_tokens": 8192},
-        "gemini-1.5-flash-8b-exp-0827": {"input": 0.075, "output": 0.30, "input_tokens": 1048576, "output_tokens": 8192}
+    #     "gemini-1.5-pro-exp-0801": {"input": 3.50, "output": 10.50, "input_tokens": 2097152, "output_tokens": 8192},
+    #     "gemini-1.5-pro-exp-0827": {"input": 3.50, "output": 10.50, "input_tokens": 2097152, "output_tokens": 8192},
+    #     "gemini-1.5-flash": {"input": 0.075, "output": 0.30, "input_tokens": 1048576, "output_tokens": 8192},
+    #     "gemini-1.5-flash-exp-0827": {"input": 0.075, "output": 0.30, "input_tokens": 1048576, "output_tokens": 8192},
+    #     "gemini-1.5-flash-8b-exp-0827": {"input": 0.075, "output": 0.30, "input_tokens": 1048576, "output_tokens": 8192}
     }
 }
