b54de560e3630dbfe2d682ec8257d9b8f708e006
dharllc <151599752+dharllc@users.noreply.github.com>
2025-04-16 11:55:12 -0700

    add new openai models

--- backend/model_config.py
+++ backend/model_config.py
@@ -1,11 +1,10 @@
 MODELS = {
     "OpenAI": {
-        # "o1-pro": {"input": 150, "output": 600, "input_tokens": 200000, "output_tokens": 100000},
-        "o1": {"input": 15, "output": 60, "input_tokens": 128000, "output_tokens": 32768},
-        "o3-mini": {"input": 1.1, "output": 4.4, "input_tokens": 200000, "output_tokens": 100000},
-        "gpt-4.5-preview": {"input": 75, "output": 150, "input_tokens": 128000, "output_tokens": 16384},
-        "gpt-4o": {"input": 2.5, "output": 10, "input_tokens": 128000, "output_tokens": 16384},
-        "gpt-4o-mini": {"input": 0.15, "output": 0.60, "input_tokens": 128000, "output_tokens": 16384}
+        "o3": {"input": 10, "output": 40, "input_tokens": 200000, "output_tokens": 100000},
+        "o4-mini": {"input": 1.1, "output": 4.4, "input_tokens": 200000, "output_tokens": 100000},
+        "gpt-4.1": {"input": 2, "output": 8, "input_tokens": 1047576, "output_tokens": 32768},
+        "gpt-4.1-mini": {"input": 0.4, "output": 1.6, "input_tokens": 1047576, "output_tokens": 32768},
+        "gpt-4.1-nano": {"input": 0.1, "output": 0.4, "input_tokens": 1047576, "output_tokens": 32768}
     },
     "Anthropic": {
         "claude-3-7-sonnet-latest": {"input": 3, "output": 15, "input_tokens": 200000, "output_tokens": 8192},
